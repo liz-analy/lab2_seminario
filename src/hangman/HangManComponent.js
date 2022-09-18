9465aaf966cf23a4caa9b357922b9b6e8d2f5780
Liz_Analy <analyqc64@gmail.com>
2022-09-17 23:46:31 -0400

    frase

--- src/hangman/HangManComponent.js
+++ src/hangman/HangManComponent.js
@@ -6,7 +6,7 @@ import { useCounter } from "./hooks/useCounter";
 import { useWord } from "./hooks/useWord";
 import "./styles.css";
 export const HangManComponent = () => {
-        const [listWord, , ifContains, isValid] = useWord("TNT");
+        const [listWord, , ifContains, isValid] = useWord("HOLA");
         const [counter, , decrementCounter] = useCounter(6, {max: 6, min: 0});
         const handlerKeyPress = (key) => {
             ifContains(key);
